2f1aa39cf569da8583210386ef37c9c9f1d44e02
Shubham Joshi <sjoshi91119@gmail.com>
2023-06-14 23:05:07 +0530

    submitting code-Wed Jun 14 2023 23:05:06 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [showModal, setShowModal] = useState(false);
 
-export default App
+  const handleButtonClick = () => {
+    setShowModal(true);
+  };
+
+  const Child = ({ showModal, handleButtonClick }) => {
+    return (
+      <div>
+        <button onClick={handleButtonClick}>Open Modal</button>
+        {showModal && <Modal />}
+      </div>
+    );
+  };
+
+  const Modal = () => {
+    return (
+      <div className="modal">
+        <h1>Modal Content</h1>
+      </div>
+    );
+  };
+
+  return (
+    <div className="parent">
+      <Child showModal={showModal} handleButtonClick={handleButtonClick} />
+    </div>
+  );
+};
+
+export default App;
